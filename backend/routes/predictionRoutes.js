9cc5dc812174b21afda416ea1037aa9cd26d7ad8
Luckymeyo <52824270+Luckymeyo@users.noreply.github.com>
2025-06-03 20:56:35 +0700

    Delete backend directory

--- backend/routes/predictionRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { comparePredictionWithStock } from '../controllers/predictionControllers.js';
-
-const router = express.Router();
-
-router.get('/compare', comparePredictionWithStock);
-
-export default router; // ✅ ES module export
