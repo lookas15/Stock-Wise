5a81d22efe14e725cfdb7e01bbce16020ba674d9
Luckymeyo <52824270+Luckymeyo@users.noreply.github.com>
2025-06-04 15:40:44 +0700

    Update register.js

--- stockwise-fe/src/pages/register.js
+++ stockwise-fe/src/pages/register.js
@@ -88,7 +88,7 @@ form.addEventListener('submit', async (e) => {
   };
 
   try {
-    const res = await fetch('http://localhost:3000/api/auth/register', {
+   const res = await fetch('https://stock-wise-production.up.railway.app/api/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
