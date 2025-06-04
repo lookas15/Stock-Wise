26a9fef34be51763050cf7b3cc68a5ab1891ee27
Luckymeyo <52824270+Luckymeyo@users.noreply.github.com>
2025-06-04 16:24:49 +0700

    Delete stockwise-fe directory

--- stockwise-fe/stockwise-fe/src/pages/distribution.js
+++
@@ -1,9 +0,0 @@
-import { renderLayout } from '../components/layout.js';
-
-export function renderDistributionPage() {
-  const app = document.getElementById('app');
-  app.innerHTML = renderLayout(`
-    <h1 class="text-4xl font-bold mb-4 text-gray-800">Distribution</h1>
-    <p class="text-lg text-gray-600">Distribution and logistics data will be shown here.</p>
-  `);
-}
